5a0589e30cb953ffa1902a6f0a471a52f6673dbb
Kevin Nguy <nguy@macbook.local>
2020-05-26 09:39:42 -0700

    Refactor app state

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, SafeAreaView, ScrollView, View } from 'react-native';
 
 import { isWeb, styleForPlatform } from '../utils/platform';
@@ -7,77 +7,105 @@ import GramInput from '../components/GramInput';
 import Slider from '../components/Slider';
 
 const TOTAL_FLOUR = 'Total flour';
-const WHOLE_WHEAT = 'Whole wheat';
+const WHOLE_WHEAT = 'Whole wheat flour';
 const WATER = 'Water';
 const STARTER = 'Starter';
 const SALT = 'Salt';
 
-const DEFAULT_INGREDIENTS = {
-  [TOTAL_FLOUR]: {
-    percent: 100,
-    gram: 300,
-  },
-  [WHOLE_WHEAT]: {
-    percent: 15,
-  },
-  [WATER]: {
-    percent: 75,
-  },
-  [STARTER]: {
-    percent: 15,
-  },
-  [SALT]: {
-    percent: 3,
-  }
-};
-
 const calculateBakerRatio = (ingredients) => {
   const { percent: totalPercent, gram: totalGram } = ingredients[TOTAL_FLOUR];
 
-  return Object.keys(ingredients).map(key => {
+  return Object.keys(ingredients).reduce((acc, key) => {
     if (key === TOTAL_FLOUR) {
-      return {
-        title: TOTAL_FLOUR,
+      acc[TOTAL_FLOUR] = {
+        id: TOTAL_FLOUR,
         percent: totalPercent,
         gram: totalGram,
       }
+
+      return acc;
     }
 
     let { percent, gram } = ingredients[key];
-    if (!percent && !gram) {
-      return {
-        title: key,
-        percent: 0,
-        gram: 0
-      }
-    }
+    // percent = Number(((gram / totalGram) * totalPercent).toFixed(1));
+    gram = Number(((percent / totalPercent) * totalGram).toFixed(1));
 
-    if (!percent) {
-      percent = Number(((gram / totalGram) * totalPercent).toFixed(1));
-    }
-
-    if (!gram) {
-      gram = Number(((percent / totalPercent) * totalGram).toFixed(1));
-    }
-
-    return {
-      title: key,
+    acc[key] = {
+      id: key,
       percent,
       gram,
     }
-  });
+
+    return acc;
+  }, {});
 }
 
+let DEFAULT_INGREDIENTS = {
+  [TOTAL_FLOUR]: {
+    id: TOTAL_FLOUR,
+    percent: 100,
+    gram: 400,
+  },
+  [WHOLE_WHEAT]: {
+    id: WHOLE_WHEAT,
+    percent: 15,
+    gram: 60,
+  },
+  [WATER]: {
+    id: WATER,
+    percent: 75,
+    gram: 300,
+  },
+  [STARTER]: {
+    id: STARTER,
+    percent: 15,
+    gram: 60,
+  },
+  [SALT]: {
+    id: SALT,
+    percent: 3,
+    gram: 12
+  }
+};
+
+DEFAULT_INGREDIENTS = calculateBakerRatio(DEFAULT_INGREDIENTS);
+
 export default function App() {
-  const bakerRatio = calculateBakerRatio(DEFAULT_INGREDIENTS);
+  const [totalFlour, setTotalFlour] = useState(DEFAULT_INGREDIENTS[TOTAL_FLOUR]);
+  const [wholeWheat, setWholeWheat] = useState(DEFAULT_INGREDIENTS[WHOLE_WHEAT]);
+  const [water, setWater] = useState(DEFAULT_INGREDIENTS[WATER]);
+  const [starter, setStarter] = useState(DEFAULT_INGREDIENTS[STARTER]);
+  const [salt, setSalt] = useState(DEFAULT_INGREDIENTS[SALT]);
+
+  const onChangeSlider = totalValueGram => {
+    setTotalFlour({ ...totalFlour, gram: totalValueGram });
+
+    const ingredients = {
+      [totalFlour.id]: totalFlour,
+      [wholeWheat.id]: wholeWheat,
+      [water.id]: water,
+      [starter.id]: starter,
+      [salt.id]: salt,
+    }
+    const bakerRatio = calculateBakerRatio(ingredients);
+    console.log(bakerRatio);
+
+    setWholeWheat(bakerRatio[wholeWheat.id]);
+    setWater(bakerRatio[water.id]);
+    setStarter(bakerRatio[starter.id]);
+    setSalt(bakerRatio[salt.id]);
+  }
+
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        {bakerRatio.map(({ title, percent, gram }) => (
-          <GramInput key={title} title={title} percentValue={percent} gramValue={gram} />
-        ))}
-        <Slider defaultValue={300} />
+        <GramInput title={totalFlour.id} percentValue={totalFlour.percent} gramValue={totalFlour.gram} />
+        <GramInput title={wholeWheat.id} percentValue={wholeWheat.percent} gramValue={wholeWheat.gram} />
+        <GramInput title={water.id} percentValue={water.percent} gramValue={water.gram} />
+        <GramInput title={starter.id} percentValue={starter.percent} gramValue={starter.gram} />
+        <GramInput title={salt.id} percentValue={salt.percent} gramValue={salt.gram} />
+        <Slider defaultValue={300} onChange={onChangeSlider} />
       </ScrollView>
     </SafeAreaView>
   );
@@ -93,7 +121,6 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
       },
       web: {
-        marginTop: 40,
         alignItems: 'center'
       }
     })
